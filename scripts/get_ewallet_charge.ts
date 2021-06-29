ff577f852cacbf587eebaf1181bdc4f655a1df17
Teerawat Lamanchart <teerawat@zipmex.com>
2021-06-30 01:46:54 +0700

    Remove types

--- scripts/get_ewallet_charge.ts
+++ scripts/get_ewallet_charge.ts
@@ -1,8 +1,8 @@
 import { getAPIKey, getUserInput } from "./helpers.ts";
 import { XenditClient } from "../lib/xendit_client.ts";
 
-const apiKey: string = getAPIKey();
-const chargeID: string = getUserInput("charge id:");
+const apiKey = getAPIKey();
+const chargeID = getUserInput("charge id:");
 
 const client = new XenditClient(apiKey);
 const json = await client.getEWalletCharge(chargeID);
--- scripts/simulate_va_payment.ts
+++ scripts/simulate_va_payment.ts
@@ -1,10 +1,10 @@
 import { getAPIKey, getUserInput } from "./helpers.ts";
 import { XenditClient } from "../lib/xendit_client.ts";
 
-const apiKey: string = getAPIKey();
-const bankCode: string = getUserInput("bank code:");
-const bankAccountNumber: string = getUserInput("bank account number:");
-const transferAmount: string = getUserInput("transfer amount:");
+const apiKey = getAPIKey();
+const bankCode = getUserInput("bank code:");
+const bankAccountNumber = getUserInput("bank account number:");
+const transferAmount = getUserInput("transfer amount:");
 
 const client = new XenditClient(apiKey);
 const json = await client.simulateVAPayment(
